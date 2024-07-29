72eb06b15acf0571e4664686c9a183d5a8e72c70
Sparky <sparky@Linux>
2024-07-30 03:17:13 +0530

    Changes commit and need for updating in the frontend for handling responses

---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,15 @@
+export function CreateTodo(){
+    return <div>
+        <input style={{
+            padding:10,
+            margin:10
+        }} type="text" placeholder="title" id="" /> <br /> 
+        <input style={{
+            padding:10,
+            margin:10
+        }} type="text" placeholder="description" id="" /><br />
+        <button>Add a Todo</button>
+
+        
+    </div>
+}
